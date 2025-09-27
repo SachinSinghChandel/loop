ed7401d1a5ec6891c7a3c0ac9dc05c3801a019d6
SachinSinghChandel <sachinsinghbhp@gmail.com>
2025-09-27 21:23:34 +0530

    add commit

---
+++ loop.py
@@ -0,0 +1,57 @@
+# count=1# count is iterator
+# while count <= 10:
+#     print("sachin ")
+#     count = count+1 # count +=1
+# print("loop is end  because count =11")
+
+# i=1
+# while i<=10:
+#     print(2*i)
+# #     i +=1
+
+
+# j=100
+# while j >= 1:
+#     print(j)
+#     j-=1
+
+# n=int(input("N is :"))
+# k=1
+# while k<=10:
+#     print(n*k)
+#     k+=1
+
+# # num=(10,20,30,40,50,60,70)
+# # x=30
+# # i=0
+# # while i < len(num):
+# #     if(num[i] == x):
+# #         print("value is present in tuple in index:",i)
+# #     i+=1
+
+# # use of break keyword
+# # i=1
+# # while i <= 100:
+# #     print(i)
+# #     if(i==30):
+# #         break
+# #     i+=1
+# # print("end of loop")
+
+# # continue
+# l=0
+# while l <= 10:
+#     if(l==5): #skip
+#         l+=1
+#         continue
+#     print(l)
+#     l+=1
+
+# wap to print odd number only from 1 to 20 using continue
+o=0
+while o <= 20 :
+    if(o%2==0):
+        o+=1
+        continue
+    print(o)
+    o+=1
